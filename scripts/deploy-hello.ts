761e440be52f0451e542522ef60bd8a40a9bb1e6
Vijay Sah <vijay.11sah@gmail.com>
2023-03-13 16:05:42 +0530

    adds deploy script

---
+++ scripts/deploy-hello.ts
@@ -0,0 +1,15 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+
+async function deploy() {
+  const HelloWorld = await ethers.getContractFactory("HelloWorld");
+  const hello = await HelloWorld.deploy();
+  await hello.deployed();
+  return hello;
+}
+
+async function sayHello(hello: any) {
+  console.log(`Say Hello: ${await hello.hello()}`);
+}
+
+deploy().then(sayHello);
